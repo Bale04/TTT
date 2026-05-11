29bd4e305683cfdab012aa0053cec7b928127727
Mattia Emanuele Balestrucci <150167509+Bale04@users.noreply.github.com>
2026-05-11 18:15:30 +0200

    V 0.3
    Dichiarazione di tutte le funzioni di accesso e stampa del file di testo delle impostazioni

--- src/TTT.c
+++ src/TTT.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : TTT.c
  Author      : Mattia Emanuele Balestrucci, Vincenzo Basilio, Luigi Bonasia, Ruggiero Dicorato
- Version     :
+ Version     : V 0.3
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
@@ -11,26 +11,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 // struttura della gestione delle stringhe
-typedef struct {
-    char data[20];
-} Stringa;
+#include "TTT.h"
 
-// struttura delle impostazioni della partita
-typedef struct{
-	int modoPartita;  // 1 - CPU,   0 - Giocatore
-	Stringa nomeGiocatore1;  // nome del giocatore 1
-	char simboloGiocatore1;  // simbolo giocatore 1
-	Stringa nomeGiocatore2;  // nome del giocatore 2
-	char simboloGiocatore2;  // simbolo del giocatore 2
-	Stringa partitaPrecedente;  // nome della partita da riprendere
-	int annullaImpostazioni;  // 1 - Default,  0 - Modificate
-	int numeroRound;  // numero dei round
-	Stringa nomePartita;  // nome della partita da giocare
-}Impostazioni;
+
+
+// DEFINIZIONE FUNZIONI DI ACCESSO
+
+void Set_modoPartita(int m, Impostazioni* impostazioni);
+int Get_modoPartita(Impostazioni impostazioni);
+void Set_nomeGiocatore1(Stringa n1, Impostazioni *impostazioni);
+Stringa Get_nomeGiocatore1(Impostazioni impostazioni);
+void Set_nomeGiocatore2(Stringa n2, Impostazioni* impostazioni);
+Stringa Get_nomeGiocatore2(Impostazioni impostazioni);
+void Set_simboloGiocatore1(char s1, Impostazioni* impostazioni);
+char Get_simboloGiocatore1(Impostazioni impostazioni);
+void Set_simboloGiocatore2(char s2, Impostazioni* impostazioni);
+char Get_simboloGiocatore2(Impostazioni impostazioni);
+void Set_partitaPrecedente(Stringa p, Impostazioni *impostazioni);
+Stringa Get_partitaPrecedente(Impostazioni impostazioni);
+void Set_annullaImpostazioni(int x, Impostazioni* impostazioni);
+int Get_annullaImpostazioni(Impostazioni impostazioni);
+void Set_numeroRound(int r, Impostazioni* impostazioni);
+int Get_numeroRound(Impostazioni impostazioni);
+void Set_nomePartita(Stringa n, Impostazioni* impostazioni);
+Stringa Get_nomePartita(Impostazioni impostazioni);
+
+
+
+int main(void) {
+	// appena si avvia il codice si impostano le impostazioni di default.
+	Impostazioni impostazioni={1, {"giocatore1"}, 'X', {"giocatore2"}, 'O',{""}, 1, 3, {"partita"}};
+	FILE *fpImpostazioni;
+	int c;
+
+	// apre il file in lettura per caricare la schermata di impostazioni
+	fpImpostazioni = fopen("Impostazioni.txt", "r");
+	if (fpImpostazioni == NULL) {
+		printf("Errore Caricamento Impostazioni\n");
+	} else {
+		// finchè non raggiunge la fine del file legge i caratteri man mano e li stampa a schermo
+		while ((c = fgetc(fpImpostazioni)) != EOF) {
+			putchar(c);
+		}
+		fclose(fpImpostazioni);
+		printf("\n");
+	}
+
+	Stringa nom = {""};
+	// Set_nomeGiocatore1(nom, &impostazioni);
+	// printf("\n%s\n", Get_nomeGiocatore1(impostazioni).data);
+	return EXIT_SUCCESS;
+}
+
+
 
 // FUNZIONI DI ACCESSO
+
+
 // ---------------------MODO PARTITA---------------------------
 void Set_modoPartita(int m, Impostazioni* impostazioni){
 	impostazioni->modoPartita = m;
@@ -68,20 +106,41 @@ char Get_simboloGiocatore2(Impostazioni impostazioni){
 	return impostazioni.simboloGiocatore2;
 }
 // ------------------PARTITA PRECEDENTE------------------------------
+void Set_partitaPrecedente(Stringa p, Impostazioni *impostazioni){
+	strncpy(impostazioni->nomeGiocatore2.data, p.data, sizeof(impostazioni->nomeGiocatore2.data) - 1);
 
+}
+Stringa Get_partitaPrecedente(Impostazioni impostazioni){
+	return impostazioni.partitaPrecedente;
+}
 // ---------------------IMPOSTAZIONI DEFAULT---------------------------
-
+void Set_annullaImpostazioni(int x, Impostazioni* impostazioni){
+	impostazioni->annullaImpostazioni = x;
+	if (x==1){
+		impostazioni->modoPartita = 1;
+		strncpy(impostazioni->nomeGiocatore1.data, "giocatore1", sizeof(impostazioni->nomeGiocatore1.data) - 1);
+		impostazioni->simboloGiocatore1 = 'X';
+		strncpy(impostazioni->nomeGiocatore2.data, "giocatore2", sizeof(impostazioni->nomeGiocatore2.data) - 1);
+		impostazioni->simboloGiocatore2 = 'O';
+		strncpy(impostazioni->partitaPrecedente.data, "", sizeof(impostazioni->partitaPrecedente.data) - 1);
+		impostazioni->numeroRound = 3;
+		strncpy(impostazioni->nomePartita.data, "partita", sizeof(impostazioni->nomePartita.data) - 1);
+	}
+}
+int Get_annullaImpostazioni(Impostazioni impostazioni){
+	return impostazioni.annullaImpostazioni;
+}
 // ---------------------ROUND DA GIOCARE---------------------------
-
+void Set_numeroRound(int r, Impostazioni* impostazioni){
+	impostazioni->numeroRound = r;
+}
+int Get_numeroRound(Impostazioni impostazioni){
+	return impostazioni.numeroRound;
+}
 // ---------------------NOME PARTITA---------------------------
-
-
-
-int main(void) {
-	// appena si avvia il codice si impostano le impostazioni di default.
-	Impostazioni impostazioni={1, {"giocatore1"}, 'X', {"giocatore2"}, 'O',{""}, 1, 3, {"partita"}};
-	Stringa pazz= {"NIGGERS\n"};
-	Set_nomeGiocatore1(pazz, &impostazioni);
-	printf("%s", Get_nomeGiocatore1(impostazioni).data);
-	return EXIT_SUCCESS;
+void Set_nomePartita(Stringa n, Impostazioni* impostazioni){
+	strncpy(impostazioni->nomePartita.data, n.data, sizeof(impostazioni->nomePartita.data) - 1);
+}
+Stringa Get_nomePartita(Impostazioni impostazioni){
+	return impostazioni.nomePartita;
 }
---
+++ src/TTT.h
@@ -0,0 +1,25 @@
+
+
+#ifndef TTT_H
+#define TTT_H
+
+// Definizione della stringa di caratteri
+typedef struct {
+    char data[20];
+} Stringa;
+
+// struttura delle impostazioni della partita
+typedef struct{
+	int modoPartita;  // 1 - CPU,   0 - Giocatore
+	Stringa nomeGiocatore1;  // nome del giocatore 1
+	char simboloGiocatore1;  // simbolo giocatore 1
+	Stringa nomeGiocatore2;  // nome del giocatore 2
+	char simboloGiocatore2;  // simbolo del giocatore 2
+	Stringa partitaPrecedente;  // nome della partita da riprendere
+	int annullaImpostazioni;  // 1 - Default,  0 - Modificate
+	int numeroRound;  // numero dei round
+	Stringa nomePartita;  // nome della partita da giocare
+}Impostazioni;
+
+
+#endif /* TTT_H */
